d17ee6db86fc96a60976cf572c5e9fc3c3651014
Reborn Dzre <reborndzre@gmail.com>
2021-05-02 20:49:16 +0000

    Initial Commit

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import CarItem from "./components/CarItem";
-import StyledButton from "./components/StylesButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem/>
+      <CarItem
+               name={"Model X"}
+               tagline={"Order Online For "}
+               taglineCTA={"Touchless Delivery"}
+               image={require('./assets/images/ModelX.jpeg')}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,25 +4,33 @@ import styles from "./styles";
 import StyledButton from "../StylesButton";
 
 const CarItem = (props) => {
+    const {name,tagline,taglineCTA,image} = props
     return (
         <View style={styles.carContainer}>
-            <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image} />
+            <ImageBackground source={image} style={styles.image} />
             <View style={styles.titles}>
-                <Text style={styles.title}>Model S</Text>
-                <Text style={styles.subtitles}>Starting at $69, 876</Text>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subtitles}>
+                    {tagline}
+                    <Text style={styles.subtitleCTA}>
+                        {taglineCTA}
+                    </Text>
+                </Text>
             </View>
-            <StyledButton
-                type="primary"
-                content={"Custom Order"}
-                onPress={() => {
-             alert('Custom Order Was Press')
-            }} />
-            <StyledButton
-                  type="secondary"
-                  content={"Existing Inventory"}
-                  onPress={() => {
-             alert('Existing Inventory Was Press')
-            }} />
+            <View style={styles.buttonContainer}>
+                <StyledButton
+                    type="primary"
+                    content={"Custom Order"}
+                    onPress={() => {
+                        alert('Custom Order Was Press')
+                    }} />
+                <StyledButton
+                    type="secondary"
+                    content={"Existing Inventory"}
+                    onPress={() => {
+                        alert('Existing Inventory Was Press')
+                    }} />
+            </View>
         </View>
     );
 };
